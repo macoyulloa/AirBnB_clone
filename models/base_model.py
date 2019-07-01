0396570506823350d366fd768716040ed840519c
Mariana <marianaplazas@live.com>
2019-07-01 20:34:44 +0000

    [FIXES] base for the five point

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,7 @@ this is parent class that inherates to the other class
 
 import uuid
 from datetime import datetime
-#from models import storage
+from models import storage
 import json
 
 
@@ -16,23 +16,25 @@ class BaseModel:
     'define the base model class'
 
     def __init__(self, *args, **kwargs):
-        'init with args and kwgards'    
+        'init with args and kwgards'  
         if kwargs:
-            for key, value in kwargs:
-                if key in "created_at":
-                    self.created_at = datetime.strptime(kwargs[created_at], time)
-                if key in "updated_at":
-                    self.updated_at = datetime.strptime(kwargs[updated_at], time)
+            self.__dict__ = kwargs
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if "updated_at" in kwargs:
+                self.update_at = datetime.strptime(kwargs["updated_at"], time)
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
- #           storage.new(self)
-  #          storage.save()
+            self.update_at = datetime.now()
+            storage.new(self)
+            storage.save()
             
     def save(self):
         'public method for update datetime'
         self.updated_at = datetime.now()
-   #     storage.save()
+        storage.save()
 
     def to_dict(self):
         'dictionary format'
