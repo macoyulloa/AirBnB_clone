ccdffc7da42b568660f86e9c857746ee467b0e86
Mariana <marianaplazas@live.com>
2019-07-02 14:02:17 +0000

    [FIXES] user

--- models/user.py
+++ models/user.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-class User(BaseModel):
-" class User that inherits from BaseModel "
+from models.base_model import BaseModel
+"""
+ class User that inherits from BaseModel
+"""
 
-    def __init__(self, email, password, first_name, last_name)
-        " public class attributes " 
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
+
+class User(BaseModel):
+    " public class attributes " 
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
